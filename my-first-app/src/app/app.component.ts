5852cade70fe44888811ad0d06e2cbb7d194d21b
Tommy18062001 <johantony63@gmail.com>
2022-05-01 07:50:56 +0300

    Week 2

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
   name = 'Tommy';
+  userName = '';
+  isClicked = false;
+
+  updateStatus() {
+    this.isClicked = false ? this.isClicked = true : this.isClicked = true;
+  }
 }
