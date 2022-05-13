375e4b086e0c4c9fe02fc190330fa17b7b8f12e3
Tommy18062001 <johantony63@gmail.com>
2022-05-13 07:21:47 +0300

    add link to one of the document object

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -8,7 +8,7 @@ import { Document } from '../document.model';
 })
 export class DocumentListComponent implements OnInit {
   documents: Document[] = [
-    new Document("WDD 130: Introduction to Web", "2", "Study the basic components of web and start create your first web page.", "#", []),
+    new Document("WDD 130: Introduction to Web", "2", "Study the basic components of web and start create your first web page.", "https://catalog.byu.edu/business/information-systems/introduction-web-development", []),
     new Document("CSE 340: Web Backend Development", "3", "Use PHP to interact with the web server", "#", []),
     new Document("ENG 150: Writing and reasoning Foundation", "4", "Improve your writing skills", "#", []),
     new Document("WDD 430: Web Full Stack Development", "5", "This course will teach students how to design and build interactive web based applications using HTML CSS, JavaScript, and a web development stack (3 credits).", "#", []),
